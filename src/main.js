59bb33e76152fa972d5cd8da3c7127e201072914
yunishimura0716 <yunishimura0716@gmail.com>
2021-08-06 14:48:50 +0900

    added hover for border

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import MaterialKit from './material-kit';
 import VWave from 'v-wave'
 import vuetify from './globalComponents/vuetify'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faNode } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add(fas, far, faNode);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 Vue.use(MaterialKit);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,18 +6,25 @@
     <v-divider class="mx-4"></v-divider>
     <div id="project">
       <h2 class="subtitle">Projects</h2>
-      <div class="project-cards" v-for="(project) in projectList" :key="project.title">
+      <div
+        class="project-cards"
+        v-for="(project) in projectList"
+        :key="project.title"
+      >
         <v-card
-            elevation="5"
-            outlined
-            shaped
-            max-width="800"
-            max-height="350"
-            class="project-card mx-auto my-12"
+          elevation="5"
+          outlined
+          shaped
+          max-width="800"
+          max-height="330"
+          class="project-card mx-auto my-12"
+          :class="{selected:activeProject === project.title}"
+          @mouseover="activeProject=project.title"
+          @mouseout="activeProject=''"
         >
           <v-img
-              height="250"
-              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+              height="327"
+              :src="project.img"
               class="project-card-img"
           ></v-img>
 
@@ -29,7 +36,13 @@
 
             <v-divider class="mx-4"></v-divider>
 
-            <v-card-title class="project-card-stack">Used Stack: </v-card-title>
+            <v-card-title>Used Stack:</v-card-title>
+            <div class="project-card-stack">
+              <div class="project-card-tech" v-for="stack in project.stacks" :key="stack.name">
+                <i :class="stack.class"></i>
+                <p>{{ stack.name }}</p>
+              </div>
+            </div>
           </div>
         </v-card>
       </div>
@@ -46,6 +59,7 @@ import Profile from '../components/Profile.vue';
 import Education from '../components/Education.vue';
 import Work from "../components/Work.vue";
 import Footer from "../components/Footer.vue";
+import promopal from "../assets/img/promopal.gif";
 
 export default {
   components: {
@@ -59,43 +73,69 @@ export default {
     return {
       projectList: [
         {
-          title: 'Cafe Badilico',
-          text: 'Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.',
-          stacks: [],
+          title: 'PromoPal',
+          text: 'Finding the best restaurant promotions and deals while supporting local 🍔',
+          img: promopal,
+          stacks: [
+            { name: 'PostgreSQL', class: 'fas fa-database fa-lg'},
+            { name: 'TypeORM', class: 'fas fa-tools fa-lg'},
+            { name: 'Redis', class: 'fas fa-database fa-lg'},
+            { name: 'Node.js', class: 'fab fa-node fa-lg'},
+            { name: 'Firebase', class: 'fas fa-server fa-lg'},
+            { name: 'Docker', class: 'fab fa-docker fa-lg'}
+          ],
         }
       ],
+      activeProject: "",
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
 .subtitle {
   text-align: center;
   color: black;
+  margin-bottom: 40px;
 }
 
 .project-card {
   display: grid;
   grid-template-columns: repeat(12, 1fr);
+  margin-bottom: 40px;
+}
+
+.selected {
+  border-color: #66FF33 !important;
+  border-width: 2px !important;
 }
 
 .project-card-img {
-  grid-column: 1 / span 5;
+  grid-column: 1 / span 6;
 }
 
 .project-card-body {
-  grid-column: 6 / span 7;
+  grid-column: 7 / span 7;
   display: grid;
   grid-template-rows: repeat(6, 1fr);
-  max-height: 350px;
+  max-height: 330px;
 }
 
 .project-card-title {}
 
 .project-card-text {}
 
-.project-card-stack {}
+.project-card-stack {
+  margin-left: 10px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+
+.project-card-tech {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+}
+
 
 #backimg2 {
   background-image: url("../assets/img/backimg2.jpg");
